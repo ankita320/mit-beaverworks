7badd11263a871df9748df6353a3a53a7109284c
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2022-04-17 00:39:08 -0400

    Update intro_python.py

--- intro_python.py
+++ intro_python.py
@@ -1,1 +1,24 @@
 
+from embsec import Serial
+ser = Serial('/embsec/intro_python/read_example')
+
+def read_example():
+    ser = Serial("/embsec/intro_python/read_example")
+    # Your code goes here!
+    d = ser.read()
+    while d != b'\n':
+        print(d.decode(), end='')
+        d = ser.read()
+read_example()
+
+
+rom embsec import Serial
+ser = Serial('/embsec/intro_python/write_example')
+
+def write_example():
+    ser = Serial("/embsec/intro_python/write_example")
+    # Your code goes here!
+    ser.write(b'hello world!\n')
+    print(ser.read_until())
+
+write_example()
